fb18edf3e1649fb9b225c649c3428ba82c6c947e
subhan4t <saleemsubhan2003@gmail.com>
2025-01-19 01:11:02 +0530

    Fixed css spinner styling

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -256,8 +256,8 @@ const Form = () => {
             {loader && (
               <Box sx={{
                 position: "absolute",
-                top: "50",
-                left: "50",
+                top: "50%",
+                left: "50%",
                 transform: "translate(-50%,-50%)"
               }}>
                 <Loader />
